5446f07782d946e8a82ede4639b3275017567261
Choco <trust2065@example.com>
2018-04-20 16:44:14 +1000

    強制改變字體

---
+++ buildarecipebox/src/components/Ingredient.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Ingredient extends Component {
+  render() {
+    return <input type="text" className="form-control" />;
+  }
+}
+
+export default Ingredient;
