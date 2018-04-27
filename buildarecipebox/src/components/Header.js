e60629a59960da21dc9abb3dc0c5ee01e4768dff
Choco <trust2065@example.com>
2018-04-27 21:53:58 +1000

    Header Component

---
+++ buildarecipebox/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../css/index.css";
+
+class Header extends Component {
+  render() {
+    return <div className="container">
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <Link to="/">
+              <h2>{this.props.title}</h2>
+            </Link>
+          </div>
+        </nav>
+      </div>;
+  }
+}
+export default Header;
--- buildarecipebox/src/index.js
+++ buildarecipebox/src/index.js
@@ -4,12 +4,14 @@ import './css/Recipe.css';
 // import App from "./components/App";
 import Recipe from "./components/Recipe";
 import Home from "./components/Home";
+import Header from "./components/Header";
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
     <div>
+      <Route path="/" render={() => <Header title="Site for Your Recipes" />} />
       <Route exact path="/" component={Home} />
       <Route path="/recipe/:id" component={Recipe} />
     </div>
