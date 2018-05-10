992ff8c01f72bda672d17d414050af5684a91791
Choco <trust2065@example.com>
2018-05-10 14:20:06 +1000

    fix router bug

--- buildarecipebox/src/index.js
+++ buildarecipebox/src/index.js
@@ -6,7 +6,12 @@ import Recipe from './components/Recipe';
 import Home from './components/Home';
 import Header from './components/Header';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -18,8 +23,13 @@ ReactDOM.render(
           path="/"
           render={() => <Header title="Site for Your Recipes" />}
         />
-        <Route exact path="/" component={Home} />
-        <Route path="/recipe/:id" component={Recipe} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/recipe/:id" component={Recipe} />
+          <Route>
+            <Redirect to="/" />
+          </Route>
+        </Switch>
       </div>
     </Router>
   </Provider>,
