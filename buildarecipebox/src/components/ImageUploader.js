56a5decd5488727dfbed84e4cbc45919759a23bb
Choco <trust2065@example.com>
2018-06-02 20:48:58 +1000

    remove store from dumb component

--- buildarecipebox/src/components/ImageUploader.js
+++ buildarecipebox/src/components/ImageUploader.js
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-const ImageUploader = connect(store => {
-  return {};
-})(
-  class ImageUploader extends Component {
-    render() {
-      const { no, onImageUpload, url, disabled, uploading } = this.props;
+class ImageUploader extends Component {
+  render() {
+    const { no, onImageUpload, url, disabled, uploading } = this.props;
 
-      return (
-        <div>
-          <form id="formImage">
-            {uploading ? (
-              <label className="btn btn-block mt-2">Uploading</label>
-            ) : (
-              <label
-                htmlFor={`image_${no}`}
-                type="button"
-                className="btn btn-block mt-2">
-                Select Image
-              </label>
-            )}
-            <input
-              disabled={disabled}
-              type="file"
-              id={`image_${no}`}
-              style={{ display: 'none' }}
-              accept="image/"
-              data-max-size="5000"
-              onChange={onImageUpload}
-            />
-          </form>
-          <img src={url} alt="" />
-        </div>
-      );
-    }
+    return (
+      <div>
+        <form id="formImage">
+          {uploading ? (
+            <label className="btn btn-block mt-2">Uploading</label>
+          ) : (
+            <label
+              htmlFor={`image_${no}`}
+              type="button"
+              className="btn btn-block mt-2">
+              Select Image
+            </label>
+          )}
+          <input
+            disabled={disabled}
+            type="file"
+            id={`image_${no}`}
+            style={{ display: 'none' }}
+            accept="image/"
+            data-max-size="5000"
+            onChange={onImageUpload}
+          />
+        </form>
+        <img src={url} alt="" />
+      </div>
+    );
   }
-);
+}
 
 export default ImageUploader;
